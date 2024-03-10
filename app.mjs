f74bb1370c44962b81b6426c8fb9c0692fa337e4
Alicia Lvovsky <alicialevovsky@gmail.com>
2024-03-10 18:26:32 +0200

    #2 solution

--- app.mjs
+++ app.mjs
@@ -1,42 +1,109 @@
 import express from 'express';
 import log from '@ajar/marker';
 import morgan from 'morgan';
-import dotenv from 'dotenv';
-dotenv.config();
 
+const app = express()
+
+app.use( morgan('dev'));
+
+// environment variables:
 const { PORT, HOST } = process.env;
 
 // console.log(process.env);
 
-const app = express()
-
-app.use(morgan('dev'));
 
 
-app.get('/search', (req, res, next) => {
-    const { food, town } = req.query;
-    res.status(200).json({ food, town });
-});
+// writing middleware function:
 
-app.get('/users/:id', (req, res, next) => {
-    const userId = req.params.id;
-    res.status(200).json({ userId });
-});
+const middlewareLogger = (req, res, next) => {
+    console.log('LOGGED (1) white');
+    next()
+}
 
 
-'/search?food=burger&town=ashdod'
+const middlewareLogger1 = (req,res, next) => {
+    log.yellow('LOGGED (2) yellow')
+    next()
+}
 
-app.post('/submit', express.json(), (req, res, next) => {
-    const { name, email } = req.body;
-    res.status(200).json({ name, email });
-});
+const middlewareLogger2 = (data) => (req ,res, next) => {
+    log.info(`${data} RQUESTED URL --> ${req.url}`)
+    next()
+}
+app.use(middlewareLogger)
+app.use(middlewareLogger1)
+app.use(middlewareLogger2 ("LOL OF COURSE that i'm a req url:"))
 
-app.use((req, res, next) => {
-    res.status(404).send(`404 - ${req.url} was not found`);
-});
 
-app.listen(PORT, HOST, () => {
-    log.magenta(`🌎  listening on`, `http://${HOST}:${PORT}`);
+// app.use(bodyParser.urlencoded({ extended: false}))
+
+app.use(express.json())
+
+// definition of routing section :
+
+
+app.get('/',  (req, res) => {
+    res.status(200).send(`<h2> Hello Express, Hope you’re doing well! </h2>`)
+})
+
+
+// request query:
+
+
+// http://localhost:8888/search?phone=iphone&price=3889&color=titanium
+
+app.get('/search', (req, res) => {
+    // log.blue("query str phone: " + req.query.phone)
+    // log.yellow("query str price: " + req.query.price)
+    // log.green("query str color: " + req.query.color)
+    // res.status(200).json(req.query)
+    console.log(req.query);
+    const { phone, price, color } = req.query;
+    
+    res.send({phone, price, color})
+})
+
+// -----------------------------------------------------------------
+// app.get('/html', (req, res) => {
+    // let insertData = "INSERETED"
+    
+    // const htmlMarkup = `<h1>I am a headline made with HTML</h1>
+    // <p>And I am a simple text paragraph. 
+    // The color of this text is styled with CSS. 
+    // Click the button below to remove me through the power JavaScript.</p>
+    // <button>${insertData}</button>`
+    
+    // res.status(200).set('Content-Type', 'text/html').send(htmlMarkup)
+    // })
+    // cmd+shift+R needed to refresh because of chashing
+    // ------------------------------------------------------------
+
+
+    
+    // request params:
+    
+    // '/races/monaco/08.03.2024
+
+    app.use('/races/:raceID/:date', (req, res) => {
+        res.status(200).send(`<h1> Next F1 track day in: ${req.params.raceID}</h1> <h2> date: ${req.params.date}</h2>`)
+    })
+
+
+// request body:
+    app.post('/races', (req,res,next) => {
+        res.status(200).send(`The race will be in ${req.body?.raceName}`)
+    })
+
+    // return 404 status:
+    app.use( (req,res,next) => {
+        res.status(404).send(log.red(`ERROR 404 -  INTERNET (${req.url}) NOT FOUND, check ONLINE for solutions :D `))
+    })
+
+
+
+
+app.listen(PORT, HOST,  ()=> {
+    log.blue(`🌎  listening on`,`http://${HOST}:${PORT}`);
 });
 
 
